c113229839f15b5125cb6509da5d493c962fd7a2
Naoufal ALAOUI ISMAILI <alaouiismailinaoufal@gmail.com>
2021-09-23 20:21:18 +0200

    Premier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPipe } from './search.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(list: any[], value: any[], key: any[]): any {
+    value.forEach((name, index) => {
+      if (name) {
+        list = list.filter((item) => {
+          return (item[key[index]]
+            .toString()
+            .toLowerCase()
+            .indexOf(name.toString().toLowerCase()) !== -1)
+        });
+      }
+    });
+    return list;
+  }
+
+  // transform(value: unknown, ...args: unknown[]): unknown {
+  //   return null;
+  // }
+
+}
